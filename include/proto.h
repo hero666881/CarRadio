8daad4004e9af8b368eafdeda360d20f7944dc79
hero666881 <2958869187@qq.com>
2026-06-22 14:26:56 +0800

    🎉 Initial commit: 添加了完整的核心代码与 README 文档

--- include/proto.h
+++ include/proto.h
@@ -9,7 +9,7 @@
 #define __PROTO_H__
 
 /* ======== TCP 网络配置 ======== */
-#define SERVER_IP       "127.0.0.1"     // 本地测试 IP
+#define SERVER_IP       "10.204.75.234"     // 本地测试 IP
 #define SERVER_PORT     8888            // TCP 服务监听端口
 
 /* ======== 本地 IPC 配置 ======== */
